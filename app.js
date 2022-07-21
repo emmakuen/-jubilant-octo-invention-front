039b257200fc86e78356d64d38c2b6aa84a02087
emmakuen <emmaku@protonmail.com>
2022-07-21 13:32:14 +0900

    create app config

---
+++ app.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+const path = require("path");
+const express = require("express");
+const hbs = require("hbs");
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "./views/layouts"));
+hbs.registerPartials(path.join(__dirname, "./views/partials"));
+
+app.use(express.static(path.join(__dirname, "./public")));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`App is running on ${PORT}...`);
+});
