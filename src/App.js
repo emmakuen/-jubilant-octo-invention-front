b7c36f919cd66235695ec8375a1ef37296664eb0
emmakuen <emmaku@protonmail.com>
2022-07-25 23:15:14 +0900

    migrate and refactor 404 page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { Comments, Users } from "./views";
+import { Comments, Users, NotFound } from "./views";
 import { UsersProvider } from "./contexts/useUsers";
 import { CommentsProvider } from "./contexts/useComments";
 import GlobalStyles from "./styles/GlobalStyles";
@@ -15,6 +15,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Users title="Choose your profile" />} />
             <Route path="/comments" element={<Comments title="Discussion" />} />
+            <Route path="*" element={<NotFound title="Page Not Found" />} />
           </Routes>
         </BrowserRouter>
         <ToastContainer />
